83387b15f6e6004fa253f96f3ace276cf606cd7c
Yaroslav Ovcharenko <dzharvis@gmail.com>
2019-10-25 14:22:14 +0300

    Make bandwidth calc thread safe

--- src/main/kotlin/utils/Bandwidth.kt
+++ src/main/kotlin/utils/Bandwidth.kt
@@ -2,18 +2,23 @@ package utils
 
 data class BandwidthBucket(val time: Long, val value: Long)
 
-class BandwidthCalculator(val timeWindow: Long = 5000) {
-    private val bandwidthWindow = mutableListOf<BandwidthBucket>()
+class BandwidthCalculator(private val timeWindow: Long = 5000) {
+    private val bandwidthWindow = ArrayList<BandwidthBucket>()
 
-    // Can potentially grow large in size, but whatever
+    @Synchronized
     fun record(value: Long) {
-        val currentTime = System.currentTimeMillis()
-        bandwidthWindow.removeIf { it.time < currentTime - timeWindow }
+        val currentTimeMillis = System.currentTimeMillis()
+        bandwidthWindow.removeIf { it.time < (currentTimeMillis - timeWindow) }
         bandwidthWindow.add(BandwidthBucket(System.currentTimeMillis(), value))
     }
 
+    @Synchronized
     fun getBandwidth(): Long {
-        val currentTime = System.currentTimeMillis()
-        return bandwidthWindow.fold(0L, { acc, bucket -> acc + bucket.value }) / 5
+        if (bandwidthWindow.isEmpty()) return 0L
+        val currentTimeMillis = System.currentTimeMillis()
+        val windowMs = (currentTimeMillis - bandwidthWindow[0].time).coerceAtLeast(1)
+        val bytesReceived = bandwidthWindow.fold(0L, { acc, bucket -> acc + bucket.value })
+
+        return (bytesReceived / (windowMs / 1000.0)).toLong()
     }
 }
